3c3cee280e6cfbb72bd1663f4d5d50402b6bf152
Adarsh Verma <adarshverma2521@gmail.com.com>
2025-03-22 06:12:27 +0000

    commit in master

---
+++ test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+#Create a dir with 1 file
+#
+while true;
+do 
+	read -p "Enter the Dir Name : " dir_name
+	if [[ -z "$dir_name" ]]; then
+		echo "Error: Parent directory name cannot be empty."
+        	exit 1
+    	elif [[ -e "$dir_name" ]]; then
+        	echo "Error: '$dir_name' already exists. Enter a unique name."
+    	else
+        	mkdir -p "$dir_name"
+        	break
+    	fi
+done
+ while true;
+        do
+            read -p "Enter File Name: " file_name
+
+            if [[ -z "$file_name" ]]; then
+                echo "Error: File name cannot be empty."
+                exit 1
+            elif [[ -e "$dir_name/$file_name" ]]; then
+                echo "Error: '$file_name' already exists in '$dir_name'. Enter a unique name."
+            else
+                touch "$dir_name/$file_name"
+                break
+            fi
+        done
